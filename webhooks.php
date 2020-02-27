352a6522599b7e5c52f1a73414d961ba9f8d0b31
kamornpisit <kamornpisit03@gmail.com>
2020-02-27 13:03:34 +0700

    normal hooks

--- webhooks.php
+++ webhooks.php
@@ -53,77 +53,7 @@ if (!is_null($events['events'])) {
 				'messages' => [$messages]
 			];
 
-            // json CURLOPT_POSTFIELDS
-            $json_quick = '{
-                  "to": '.$event['source']['userId'].',
-                  "messages": [
-                    {
-                      "type": "text",
-                      "text": "Hello Quick Reply!",
-                      "quickReply": {
-                        "items": [
-                          {
-                            "type": "action",
-                            "action": {
-                              "type": "cameraRoll",
-                              "label": "Camera Roll"
-                            }
-                          },
-                          {
-                            "type": "action",
-                            "action": {
-                              "type": "camera",
-                              "label": "Camera"
-                            }
-                          },
-                          {
-                            "type": "action",
-                            "action": {
-                              "type": "location",
-                              "label": "Location"
-                            }
-                          },
-                          {
-                            "type": "action",
-                            "imageUrl": "https://cdn1.iconfinder.com/data/icons/mix-color-3/502/Untitled-1-512.png",
-                            "action": {
-                              "type": "message",
-                              "label": "Message",
-                              "text": "Hello World!"
-                            }
-                            },
-                          {
-                            "type": "action",
-                            "action": {
-                              "type": "postback",
-                              "label": "Postback",
-                              "data": "action=buy&itemid=123",
-                              "displayText": "Buy"
-                            }
-                            },
-                          {
-                            "type": "action",
-                            "imageUrl": "https://icla.org/wp-content/uploads/2018/02/blue-calendar-icon.png",
-                            "action": {
-                              "type": "datetimepicker",
-                              "label": "Datetime Picker",
-                              "data": "storeId=12345",
-                              "mode": "datetime",
-                              "initial": "2018-08-10t00:00",
-                              "max": "2018-12-31t23:59",
-                              "min": "2018-08-01t00:00"
-                            }
-                          }
-                        ]
-                      }
-                    }
-                   ]
-                }';
-
-
 			$post = json_encode($data);
-
-           // $post = $json_quick;
 
 
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
