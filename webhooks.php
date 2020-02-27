f35cc9490b126d2ee673cbd89fbccac3b4bed21f
kamornpisit <kamornpisit03@gmail.com>
2020-02-27 10:43:42 +0700

    change message

--- webhooks.php
+++ webhooks.php
@@ -20,11 +20,85 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
-			// Build message to reply back
+
+
+			$data = '{
+              "messages": [
+                {
+                  "type": "text",
+                  "text": "Hello Quick Reply!",
+                  "quickReply": {
+                    "items": [
+                      {
+                        "type": "action",
+                        "action": {
+                          "type": "cameraRoll",
+                          "label": "Camera Roll"
+                        }
+                      },
+                      {
+                        "type": "action",
+                        "action": {
+                          "type": "camera",
+                          "label": "Camera"
+                        }
+                      },
+                      {
+                        "type": "action",
+                        "action": {
+                          "type": "location",
+                          "label": "Location"
+                        }
+                      },
+                      {
+                        "type": "action",
+                        "imageUrl": "https://cdn1.iconfinder.com/data/icons/mix-color-3/502/Untitled-1-512.png",
+                        "action": {
+                          "type": "message",
+                          "label": "Message",
+                          "text": "Hello World!"
+                        }
+                        },
+                      {
+                        "type": "action",
+                        "action": {
+                          "type": "postback",
+                          "label": "Postback",
+                          "data": "action=buy&itemid=123",
+                          "displayText": "Buy"
+                        }
+                        },
+                      {
+                        "type": "action",
+                        "imageUrl": "https://icla.org/wp-content/uploads/2018/02/blue-calendar-icon.png",
+                        "action": {
+                          "type": "datetimepicker",
+                          "label": "Datetime Picker",
+                          "data": "storeId=12345",
+                          "mode": "datetime",
+                          "initial": "2018-08-10t00:00",
+                          "max": "2018-12-31t23:59",
+                          "min": "2018-08-01t00:00"
+                        }
+                      }
+                    ]
+                  }
+                }
+               ]
+            }';
+
+
+			// Build message to reply back // code!
 			$messages = [
 				'type' => 'text',
 				'text' => $text
 			];
+
+
+
+            // example! replacement
+            $messages = json_decode($data);
+
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
