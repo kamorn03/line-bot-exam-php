6787707d412214d4c550b2759ab3a8e84893abf5
kamornpisit <kamornpisit03@gmail.com>
2020-02-27 11:16:04 +0700

    hook id

--- webhooks.php
+++ webhooks.php
@@ -37,7 +37,7 @@ if (!is_null($events['events'])) {
 			$messages = [
 				'type' => 'text',
 				'text' => $text,
-                'quickReply' => $data
+                //'quickReply' => $data
             ];
 
 
