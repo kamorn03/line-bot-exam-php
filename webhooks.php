c4ed2f6dabea08f81f49047a4182418253245957
kamornpisit <kamornpisit03@gmail.com>
2020-02-27 10:52:28 +0700

    hook quick

--- webhooks.php
+++ webhooks.php
@@ -39,11 +39,6 @@ if (!is_null($events['events'])) {
             ];
 
 
-
-            // example! replacement
-            $messages = json_decode($data);
-
-
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
