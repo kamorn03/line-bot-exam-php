f978877617a67b620ad7c5f8d3619b3621245a6c
kamornpisit <kamornpisit03@gmail.com>
2020-02-27 11:40:21 +0700

    change rollback

--- webhooks.php
+++ webhooks.php
@@ -43,73 +43,6 @@ if (!is_null($events['events'])) {
                 //'quickReply'=> $quick_reply
 			];
 
-			$json = "{
-              \"to\": \"Uf666d81fbe662a6910f51d2d98046d32\",
-              \"messages\": [
-                {
-                  \"type\": \"text\",
-                  \"text\": \"Hello Quick Reply!\",
-                  \"quickReply\": {
-                    \"items\": [
-                      {
-                        \"type\": \"action\",
-                        \"action\": {
-                          \"type\": \"cameraRoll\",
-                          \"label\": \"Camera Roll\"
-                        }
-                      },
-                      {
-                        \"type\": \"action\",
-                        \"action\": {
-                          \"type\": \"camera\",
-                          \"label\": \"Camera\"
-                        }
-                      },
-                      {
-                        \"type\": \"action\",
-                        \"action\": {
-                          \"type\": \"location\",
-                          \"label\": \"Location\"
-                        }
-                      },
-                      {
-                        \"type\": \"action\",
-                        \"imageUrl\": \"https://cdn1.iconfinder.com/data/icons/mix-color-3/502/Untitled-1-512.png\",
-                        \"action\": {
-                          \"type\": \"message\",
-                          \"label\": \"Message\",
-                          \"text\": \"Hello World!\"
-                        }
-                        },
-                      {
-                        \"type\": \"action\",
-                        \"action\": {
-                          \"type\": \"postback\",
-                          \"label\": \"Postback\",
-                          \"data\": \"action=buy&itemid=123\",
-                          \"displayText\": \"Buy\"
-                        }
-                        },
-                      {
-                        \"type\": \"action\",
-                        \"imageUrl\": \"https://icla.org/wp-content/uploads/2018/02/blue-calendar-icon.png\",
-                        \"action\": {
-                          \"type\": \"datetimepicker\",
-                          \"label\": \"Datetime Picker\",
-                          \"data\": \"storeId=12345\",
-                          \"mode\": \"datetime\",
-                          \"initial\": \"2018-08-10t00:00\",
-                          \"max\": \"2018-12-31t23:59\",
-                          \"min\": \"2018-08-01t00:00\"
-                        }
-                      }
-                    ]
-                  }
-                }
-               ]
-            }";
-
-
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
@@ -117,9 +50,8 @@ if (!is_null($events['events'])) {
 				'messages' => [$messages]
 			];
 
+			$post = json_encode($data);
 
-			//$post = json_encode($data);
-            $post = $json;
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 			$ch = curl_init($url);
