d96e909853356acc0e6b974fb09e048473c6f0b2
Thejiw <kamornpisit03@gmail.com>
2020-08-17 09:46:49 +0700

    set location back to line

--- webhooks.php
+++ webhooks.php
@@ -241,25 +241,26 @@ if (!is_null($events['events'])) {
 
             $quick = json_decode($quick);
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text." - event - [ ".$event['message']['text']." ]",
+            // Build message to reply back
+            $messages = [
+                'type' => 'text',
+                'text' => $text." - event - [ ".$event['message']['text']." ]",
                 'quickReply' => $quick
-			];
+            ];
 
             /*
                 {
                    "type":"location",
                    "label":"Location"
                 }
+                if($event['message']['text'] == 'checkin'){
+                    $messages = [
+                        "type"=> "location",
+                        "label"=> "Location"
+                    ];
+                }
             */
-			if($event['message']['text'] == 'checkin'){
-                $messages = [
-                    "type"=> "location",
-                    "label"=> "Location"
-                ];
-            }
+
 
 
 			// Make a POST Request to Messaging API to reply to sender
@@ -281,7 +282,36 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
-		}
+
+
+		}else{
+
+            // Build message to reply back
+            $messages = [
+                'type' => 'text',
+                'text' => $event['type']." - event - [ ".json_encode($events)." ]",
+            ];
+
+            // Make a POST Request to Messaging API to reply to sender
+            $url = 'https://api.line.me/v2/bot/message/reply';
+            $data = [
+                'replyToken' => $replyToken,
+                'messages' => [$messages],
+            ];
+            $post = json_encode($data);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+        }
+
 	}
 }
 echo "OK";
